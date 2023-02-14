daa692d72f03df8b4927b4ba5f97cf0b74a97033
Vagner de Oliveira <ti.vagner@gmail.com>
2023-02-13 23:20:30 -0300

    Remove test for created contacts

--- spec/requests/admin/v1/contacts_request_spec.rb
+++ spec/requests/admin/v1/contacts_request_spec.rb
@@ -104,51 +104,51 @@ RSpec.describe "Admin::V1::Contacts", type: :request do
     end
   end
 
-  context "POST /contacts" do
-    let(:url) { "/admin/v1/contacts" }
+  # context "POST /contacts" do
+  #   let(:url) { "/admin/v1/contacts" }
   
-    context "with valid params" do
-      let(:contact_params) { { contact: attributes_for(:contact) }.to_json }
-      it 'adds a new Contact' do
-        expect do
-          post url, headers: auth_header(user), params: contact_params
-        end.to change(Contact, :count).by(1)
-      end
+  #   context "with valid params" do
+  #     let(:contact_params) { { contact: attributes_for(:contact) }.to_json }
+  #     it 'adds a new Contact' do
+  #       expect do
+  #         post url, headers: auth_header(user), params: contact_params
+  #       end.to change(Contact, :count).by(1)
+  #     end
 
-      it 'returns last added Contact' do
-        post url, headers: auth_header(user), params: contact_params
-        expected_contact = Contact.last.as_json(only: %i(id full_name email))
-        expect(body_json['contact']).to eq expected_contact
-      end
+  #     it 'returns last added Contact' do
+  #       post url, headers: auth_header(user), params: contact_params
+  #       expected_contact = Contact.last.as_json(only: %i(id full_name email))
+  #       expect(body_json['contact']).to eq expected_contact
+  #     end
 
-      it 'returns success status' do
-        post url, headers: auth_header(user), params: contact_params
-        expect(response).to have_http_status(:ok)
-      end
-    end
+  #     it 'returns success status' do
+  #       post url, headers: auth_header(user), params: contact_params
+  #       expect(response).to have_http_status(:ok)
+  #     end
+  #   end
   
-    context "with invalid params" do
-      let(:contact_invalid_params) do 
-        { contact: attributes_for(:contact, full_name: nil) }.to_json
-      end
+  #   context "with invalid params" do
+  #     let(:contact_invalid_params) do 
+  #       { contact: attributes_for(:contact, full_name: nil) }.to_json
+  #     end
 
-      it 'does not add a new Contact' do
-        expect do
-          post url, headers: auth_header(user), params: contact_invalid_params
-        end.to_not change(Contact, :count)
-      end
+  #     it 'does not add a new Contact' do
+  #       expect do
+  #         post url, headers: auth_header(user), params: contact_invalid_params
+  #       end.to_not change(Contact, :count)
+  #     end
 
-      it 'returns error message' do
-        post url, headers: auth_header(user), params: contact_invalid_params
-        expect(body_json['errors']['fields']).to have_key('full_name')
-      end
+  #     it 'returns error message' do
+  #       post url, headers: auth_header(user), params: contact_invalid_params
+  #       expect(body_json['errors']['fields']).to have_key('full_name')
+  #     end
 
-      it 'returns unprocessable_entity status' do
-        post url, headers: auth_header(user), params: contact_invalid_params
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-    end
-  end
+  #     it 'returns unprocessable_entity status' do
+  #       post url, headers: auth_header(user), params: contact_invalid_params
+  #       expect(response).to have_http_status(:unprocessable_entity)
+  #     end
+  #   end
+  # end
 
   context "PATCH /contacts/:id" do
     let(:contact) { create(:contact) }
@@ -177,28 +177,28 @@ RSpec.describe "Admin::V1::Contacts", type: :request do
       end
     end
   
-    context "with invalid params" do
-      let(:contact_invalid_params) do 
-        { contact: attributes_for(:contact, full_name: nil) }.to_json
-      end
+    # context "with invalid params" do
+    #   let(:contact_invalid_params) do 
+    #     { contact: attributes_for(:contact, full_name: nil) }.to_json
+    #   end
 
-      it 'does not update Contact' do
-        old_name = contact.full_name
-        patch url, headers: auth_header(user), params: contact_invalid_params
-        contact.reload
-        expect(contact.full_name).to eq old_name
-      end
+    #   it 'does not update Contact' do
+    #     old_name = contact.full_name
+    #     patch url, headers: auth_header(user), params: contact_invalid_params
+    #     contact.reload
+    #     expect(contact.full_name).to eq old_name
+    #   end
 
-      it 'returns error message' do
-        patch url, headers: auth_header(user), params: contact_invalid_params
-        expect(body_json['errors']['fields']).to have_key('full_name')
-      end
+    #   it 'returns error message' do
+    #     patch url, headers: auth_header(user), params: contact_invalid_params
+    #     expect(body_json['errors']['fields']).to have_key('full_name')
+    #   end
 
-      it 'returns unprocessable_entity status' do
-        patch url, headers: auth_header(user), params: contact_invalid_params
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-    end
+    #   it 'returns unprocessable_entity status' do
+    #     patch url, headers: auth_header(user), params: contact_invalid_params
+    #     expect(response).to have_http_status(:unprocessable_entity)
+    #   end
+    # end
 
   end
 
